ed2e7ce961c35969031660403a1646046d40a435
Adrian Scripca <benishor@gmail.com>
2014-12-30 12:41:58 +0200

    Fixed sdl2 mixer support for windows

--- cmake/FindSDL2_mixer.cmake
+++ cmake/FindSDL2_mixer.cmake
@@ -5,12 +5,27 @@
 #  SDL2_MIXER_LIBRARIES - Libraries needed to link against SDL2_mixer
 
 find_path(SDL2_MIXER_INCLUDE_DIR SDL_mixer.h
-  PATH_SUFFIXES include/SDL2 include
+  PATH_SUFFIXES include/SDL2 include SDL2
+	i686-w64-mingw32/include/SDL2
+	i686-w64-mingw32/include
+	x86_64-w64-mingw32/include/SDL2
+	x86_64-w64-mingw32/include
+	PATHS
+	/opt/cross-tools
+	~/Library/Frameworks
+	/Library/Frameworks
+	/usr/local/include/SDL2
+	/usr/include/SDL2
+	/sw # Fink
+	/opt/local # DarwinPorts
+	/opt/csw # Blastwave
+	/opt
 )
 
 find_library(SDL2_MIXER_LIBRARY
   NAMES SDL2_mixer
   PATH_SUFFIXES lib
+    i686-w64-mingw32/lib
 )
 
 set(SDL2_MIXER_LIBRARIES ${SDL2_MIXER_LIBRARY})
