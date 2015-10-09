f9661d4199e0591c82891310ef47ab0c01932aff
Adrian Scripca <benishor@gmail.com>
2015-10-09 07:43:28 +0300

    Updated stb_truetype.

--- src/framework/Font.cpp
+++ src/framework/Font.cpp
@@ -8,25 +8,31 @@
 #include <memory>
 
 namespace Acidrain {
+
+    static const int TEXTURE_SIZE = 512;
+
     Font::Font(const char* const fontFile, float fontSize) {
         HEIGHT_MAGIC_OFFSET = fontSize * 3.0f / 4.0f;
 
         string fontContent = FILESYS.getFileContent(fontFile);
-        unsigned char* bitmap = new unsigned char[512 * 512];
+        unsigned char* bitmap = new unsigned char[TEXTURE_SIZE * TEXTURE_SIZE];
 
         this->fontSize = fontSize;
 
         stbtt_BakeFontBitmap((unsigned char const*) fontContent.c_str(), 0,
                              fontSize,
                              bitmap,
-                             512, 512,
+                             TEXTURE_SIZE, TEXTURE_SIZE,
                              32, 96,
                              characterData);
 
         glGenTextures(1, &atlasId);
         glBindTexture(GL_TEXTURE_2D, atlasId);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, 512, 512, 0, GL_ALPHA, GL_UNSIGNED_BYTE, bitmap);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, TEXTURE_SIZE, TEXTURE_SIZE, 0, GL_ALPHA, GL_UNSIGNED_BYTE, bitmap);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        glGenerateMipmap(GL_TEXTURE_2D);
 
         delete[] bitmap;
     }
@@ -49,7 +55,7 @@ namespace Acidrain {
         while (*text) {
             if (static_cast<unsigned char>(*text) >= 32 && static_cast<unsigned char>(*text) < 128) {
                 stbtt_aligned_quad q;
-                stbtt_GetBakedQuad(characterData, 512, 512, *text - 32, &x, &y, &q, 1);//1=opengl,0=old d3d
+                stbtt_GetBakedQuad(characterData, TEXTURE_SIZE, TEXTURE_SIZE, *text - 32, &x, &y, &q, 1);//1=opengl,0=old d3d
 
                 glTexCoord2f(q.s0, q.t0);
                 glVertex2f(q.x0, q.y0 + HEIGHT_MAGIC_OFFSET);
